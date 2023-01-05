a012de8ae8c38accd77115579eb74f4ddbf146ca
Icyeong <dbcjswjs@gmail.com>
2023-01-06 07:29:07 +0900

    🔧 초기설정: 구성 파일 추가

---
+++ .next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    compiler: {
+      styledComponents: true,
+    },
+  };
+const withImages = require('next-images');
+module.exports = withImages();
+
+  
+  module.exports = nextConfig;
